1e0c15ab9620939f2912fdab486248f66453b87f
Dexx Stexx <drewpad@icloud.com>
2024-07-02 15:45:12 -0700

    Add missing config properties

--- static/config.py
+++ static/config.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 version = open("/usr/share/emote/version", "r").read()
 
+is_wayland = os.environ.get("XDG_SESSION_TYPE", "").lower() == "wayland"
+is_debug = os.environ.get("GTK_DEBUG") == "interactive"
+is_dev = os.environ.get("ENV") == "dev"
+
 css_path = "/usr/share/emote/style.css"
 emoji_path = "/usr/share/emote/emojis.csv"
 logo_path = "/usr/share/icons/emote.svg"
